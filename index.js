6574812ebc93ebc187b1b17ec51bfc37117fe711
Jannis R <mail@jannisr.de>
2017-05-01 00:11:10 +0200

    implementation :clap:
    thanks to @juliuste !

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 'use strict'
 
-// todo
+const normalize = require('normalize-for-search')
+const levenshtein = require('leven')
+
+const data = {
+	en: require('./english-states.json'),
+	de: require('./german-states.json')
+}
+
+const match = (name, lang = 'en') => {
+	if (!lang in data) throw new Error('invalid language')
+	const states = data[lang]
+
+	if (states[name]) return states[name]
+
+	const normalized = normalize(name)
+	if (states[normalized]) return states[normalized]
+
+	let matches = 0
+	let match = null
+	for (let state in states){
+		if (levenshtein(state, normalized) <= 2) {
+			if (matches > 0) return null
+			matches++
+			match = states[state]
+		}
+	}
+
+	return match
+}
+
+module.exports = match
